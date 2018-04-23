8fc0605ab051a11d8f0fad72eb9aad6307e96fa0
James <Icedshot@gmail.com>
2018-04-23 10:55:09 +0100

    extern "C"

--- c_shared_data.h
+++ c_shared_data.h
@@ -1,27 +1,30 @@
 #ifndef C_SHARED_DATA_H_INCLUDED
 #define C_SHARED_DATA_H_INCLUDED
 
-struct shared_data;
+extern "C"
+{
+    struct shared_data;
 
-typedef shared_data* c_shared_data;
+    typedef shared_data* c_shared_data;
 
-c_shared_data sd_alloc();
-void sd_destroy(c_shared_data data);
+    c_shared_data sd_alloc();
+    void sd_destroy(c_shared_data data);
 
-void sd_set_auth(c_shared_data data, const char* auth);
+    void sd_set_auth(c_shared_data data, const char* auth);
 
-int sd_has_front_read(c_shared_data data);
-int sd_has_front_write(c_shared_data data);
+    int sd_has_front_read(c_shared_data data);
+    int sd_has_front_write(c_shared_data data);
 
-char* sd_get_front_read(c_shared_data data);
-char* sd_get_front_write(c_shared_data data);
+    char* sd_get_front_read(c_shared_data data);
+    char* sd_get_front_write(c_shared_data data);
 
-void sd_add_back_write(c_shared_data data, const char* write);
-void sd_add_back_read(c_shared_data data, const char* read);
+    void sd_add_back_write(c_shared_data data, const char* write);
+    void sd_add_back_read(c_shared_data data, const char* read);
 
-void sd_set_user(c_shared_data data, const char* user);
-char* sd_get_user(c_shared_data data);
+    void sd_set_user(c_shared_data data, const char* user);
+    char* sd_get_user(c_shared_data data);
 
-void free_string(char*);
+    void free_string(char*);
+}
 
 #endif // C_SHARED_DATA_H_INCLUDED
