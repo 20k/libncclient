1575b1d812fc6bf683f4daf34973ff3c2846f427
James <Icedshot@gmail.com>
2018-04-23 20:42:05 +0100

    beginnings of moving string interop into own file

---
+++ nc_string_interop.hpp
@@ -0,0 +1,16 @@
+#ifndef NC_STRING_INTEROP_HPP_INCLUDED
+#define NC_STRING_INTEROP_HPP_INCLUDED
+
+#include "c_shared_data.h"
+
+inline
+std::string c_str_consume(char* c)
+{
+    std::string ret(c);
+
+    free_string(c);
+
+    return ret;
+}
+
+#endif // NC_STRING_INTEROP_HPP_INCLUDED
