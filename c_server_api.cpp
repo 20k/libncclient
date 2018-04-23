412791a6500b60f4f6bcf994097da5034a6e2123
James <Icedshot@gmail.com>
2018-04-23 17:54:08 +0100

    is_local_command

--- c_server_api.cpp
+++ c_server_api.cpp
@@ -19,3 +19,49 @@ char* sa_make_generic_server_command(const char* server_msg)
 
     return cpp_str_to_c(full_command);
 }
+
+bool is_local_command(const std::string& command)
+{
+    if(command == "#")
+        return true;
+
+    if(starts_with(command, "#edit "))
+        return true;
+
+    if(starts_with(command, "#edit_es6 "))
+        return true;
+
+    if(starts_with(command, "#edit_es5 "))
+        return true;
+
+    if(starts_with(command, "#open "))
+        return true;
+
+    if(starts_with(command, "#dir"))
+        return true;
+
+    if(starts_with(command, "#clear_autos"))
+        return true;
+
+    if(starts_with(command, "#autos_clear"))
+        return true;
+
+    if(starts_with(command, "#shutdown"))
+        return true;
+
+    if(starts_with(command, "#cls"))
+        return true;
+
+    if(starts_with(command, "#clear_term"))
+        return true;
+
+    if(starts_with(command, "#clear_chat"))
+        return true;
+
+    return false;
+}
+
+int sa_is_local_command(const char* server_msg)
+{
+    return is_local_command(server_msg);
+}
