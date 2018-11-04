8287220e766b9550543dced3b2a29f7b838338a9
James <Icedshot@gmail.com>
2018-11-04 03:08:03 +0000

    fix atomic_write, the issue was system buffering so the old 'fix' was only a workaround, albeit a nice one

--- nc_util.hpp
+++ nc_util.hpp
@@ -57,14 +57,25 @@ template<typename T>
 inline
 void atomic_write_all(const std::string& file, const T& data)
 {
+    if(data.size() == 0)
+        return;
+
     std::string atomic_extension = ".atom";
     std::string atomic_file = file + atomic_extension;
     std::string backup_file = file + ".back";
 
-    auto my_file = std::fstream(atomic_file, std::ios::out | std::ios::binary);
+    /*auto my_file = std::fstream(atomic_file, std::ios::out | std::ios::binary);
 
     my_file.write((const char*)&data[0], data.size());
-    my_file.close();
+    my_file.close();*/
+
+    HANDLE handle = CreateFile(atomic_file.c_str(), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+    WriteFile(handle, &data[0], data.size(), nullptr, nullptr);
+
+    FlushFileBuffers(handle);
+
+    CloseHandle(handle);
 
     if(!file_exists(file))
     {
@@ -79,7 +90,8 @@ void atomic_write_all(const std::string& file, const T& data)
 
     do
     {
-        bool err = ReplaceFileA(file.c_str(), atomic_file.c_str(), backup_file.c_str(), REPLACEFILE_IGNORE_MERGE_ERRORS, nullptr, nullptr) == 0;
+        //bool err = ReplaceFileA(file.c_str(), atomic_file.c_str(), backup_file.c_str(), REPLACEFILE_IGNORE_MERGE_ERRORS, nullptr, nullptr) == 0;
+        bool err = ReplaceFileA(file.c_str(), atomic_file.c_str(), nullptr, REPLACEFILE_IGNORE_MERGE_ERRORS, nullptr, nullptr) == 0;
 
         if(!err)
         {
